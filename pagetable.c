e720b5a0ac631eddb5801b29ff1d45ebc565f626
sysudengle <dengleshag@gmail.com>
2015-06-24 00:52:55 +0800

    v1

---
+++ pagetable.c
@@ -0,0 +1,95 @@
+#include <assert.h>
+#include <string.h> 
+#include "sim.h"
+#include "pagetable.h"
+
+// The top-level page table (also known as the 'page directory')
+pgdir_entry_t pgdir[PTRS_PER_PGDIR]; 	//4096个，也就是4k个一级索引
+
+// Counters for various events.
+// Your code must increment these when the related events occur.
+int hit_count = 0;
+int miss_count = 0;
+int ref_count = 0;
+int evict_clean_count = 0;
+int evict_dirty_count = 0;
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+/*
+ * Allocates a frame to be used for the virtual page represented by p.
+ * If all frames are in use, calls the replacement algorithm's evict_fcn to
+ * select a victim frame.  Writes victim to swap if needed, and updates 
+ * pagetable entry for victim to indicate that virtual page is no longer in
+ * (simulated) physical memory.
+ *
+ * Counters for evictions should be updated appropriately in this function.
+ */
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+void print_pagetbl(pgtbl_entry_t *pgtbl) {
+	int i;
+	int first_invalid, last_invalid;
+	first_invalid = last_invalid = -1;
+
+	for (i=0; i < PTRS_PER_PGTBL; i++) {
+		if (!(pgtbl[i].frame & PG_VALID) && 
+		    !(pgtbl[i].frame & PG_ONSWAP)) {
+			if (first_invalid == -1) {
+				first_invalid = i;
+			}
+			last_invalid = i;
+		} else {
+			if (first_invalid != -1) {
+				printf("\t[%d] - [%d]: INVALID\n",
+				       first_invalid, last_invalid);
+				first_invalid = last_invalid = -1;
+			}
+			printf("\t[%d]: ",i);
+			if (pgtbl[i].frame & PG_VALID) {
+				printf("VALID, ");
+				if (pgtbl[i].frame & PG_DIRTY) {
+					printf("DIRTY, ");
+				}
+				printf("in frame %d\n",pgtbl[i].frame >> PAGE_SHIFT);
+			} else {
+				assert(pgtbl[i].frame & PG_ONSWAP);
+				printf("ONSWAP, at offset %lu\n",pgtbl[i].swap_off);
+			}			
+		}
+	}
+	if (first_invalid != -1) {
+		printf("\t[%d] - [%d]: INVALID\n", first_invalid, last_invalid);
+		first_invalid = last_invalid = -1;
+	}
+}
+
+void print_pagedirectory() {
+	int i; // index into pgdir
+	int first_invalid,last_invalid;
+	first_invalid = last_invalid = -1;
+
+	pgtbl_entry_t *pgtbl;
+
+	for (i=0; i < PTRS_PER_PGDIR; i++) {
+		if (!(pgdir[i].pde & PG_VALID)) {
+			if (first_invalid == -1) {
+				first_invalid = i;
+			}
+			last_invalid = i;
+		} else {
+			if (first_invalid != -1) {
+				printf("[%d]: INVALID\n  to\n[%d]: INVALID\n", 
+				       first_invalid, last_invalid);
+				first_invalid = last_invalid = -1;
+			}
+			pgtbl = (pgtbl_entry_t *)(pgdir[i].pde & PAGE_MASK);
+			printf("[%d]: %p\n",i, pgtbl);
+			print_pagetbl(pgtbl);
+		}
+	}
+}
